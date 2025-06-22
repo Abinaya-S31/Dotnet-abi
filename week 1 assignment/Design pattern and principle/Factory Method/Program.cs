ff7f5bcafeadf746adec3b6bc61ecbea7e470145
Abinaya-S31 <abinayasubbu31@gmail.com>
2025-06-22 11:13:55 +0530

    Add files via upload

---
+++ week 1 assignment/Design pattern and principle/Factory Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<ProductFactory> factories = new List<ProductFactory>
+        {
+            new LaptopFactory(),
+            new SmartphoneFactory(),
+            new TabletFactory()
+        };
+
+        foreach (var factory in factories)
+        {
+            Product product = factory.CreateProduct();
+            product.ShowDetails();
+        }
+    }
+}
