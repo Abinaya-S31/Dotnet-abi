1c574f4bb4cec1a403ca2f7dc283d6cf1e354024
Abinaya-S31 <abinayasubbu31@gmail.com>
2025-06-29 08:24:09 +0530

    Delete week 2 assignment/SQLProject_DN4 directory

--- week 2 assignment/SQLProject_DN4/Exercise1_Ranking.sql
+++
@@ -1,9 +0,0 @@
-USE DN4_SQLProject;
-GO
-
--- Just view the results using ranking functions
-SELECT Name, Department, Salary,
-    ROW_NUMBER() OVER(PARTITION BY Department ORDER BY Salary DESC) AS RowNum,
-    RANK() OVER(PARTITION BY Department ORDER BY Salary DESC) AS RankNum,
-    DENSE_RANK() OVER(PARTITION BY Department ORDER BY Salary DESC) AS DenseRank
-FROM Employees;
