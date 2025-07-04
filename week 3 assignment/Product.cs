b78a684cb19cb9a05abbffe666ec5d63c811f5da
Abinaya-S31 <abinayasubbu31@gmail.com>
2025-07-04 10:59:33 +0530

    Add files via upload

---
+++ week 3 assignment/Product.cs
@@ -0,0 +1,7 @@
+﻿public class Product
+{
+    public int Id { get; set; }             // Primary key
+    public string? Name { get; set; }       // Product name
+    public int Quantity { get; set; }       // Stock quantity
+    public decimal Price { get; set; }      // Price
+}
