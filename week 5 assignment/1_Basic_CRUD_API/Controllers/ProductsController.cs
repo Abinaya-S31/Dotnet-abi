da136ce9df6097e485fd63bf6c149f95b4e19131
Abinaya-S31 <abinayasubbu31@gmail.com>
2025-07-20 22:38:51 +0530

    Add files via upload

---
+++ week 5 assignment/1_Basic_CRUD_API/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using JwtAuthWebApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product{ Id = 1, Name = "Pen" },
+                new Product{ Id = 2, Name = "Book" },
+                new Product{ Id = 3, Name = "Bag" }
+            };
+
+            return Ok(products);
+        }
+    }
+}
