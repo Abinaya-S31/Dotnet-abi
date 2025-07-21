05da17242a0214b3de7bffe31645c15876a70ea9
Abinaya-S31 <abinayasubbu31@gmail.com>
2025-07-21 21:56:27 +0530

    Add files via upload

---
+++ week 5 assignment/ JWT Authentication API/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("data")]
+        public IActionResult GetData()
+        {
+            return Ok("You are authorized!");
+        }
+    }
+}
